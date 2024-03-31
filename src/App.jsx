81623f5bb1225e65d61c86a84aab0d76c3f2b2cc
Iskander Hakimov <wham@list.ru>
2024-03-31 11:09:48 +0300

    +TMA

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h2>Vite + React</h2>
+      <h2>Vite + React + TMA</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
